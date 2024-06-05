4a4cdec7b33f7f3602a50b7d5163ee74aad42fe0
Camila Mendez <mcmendezmanotas@gmail.com>
2024-06-05 11:01:25 -0500

    adicionando un contenedor el boton

--- src/CreateTodoButton/CreateTodoButton.js
+++ src/CreateTodoButton/CreateTodoButton.js
@@ -5,7 +5,9 @@ import { TodoContext } from '../TodoContext/TodoContext';
 function CreateTodoButton() {
     const { setOpenModal } = useContext(TodoContext);
     return (
-        <button className="createTodoButton" onClick={(event)=>{setOpenModal((state=>!state))}}>+</button>
+        <div className='box-button'>
+            <button className="createTodoButton" onClick={(event)=>{setOpenModal((state=>!state))}}>+</button>
+        </div>
     )
 }
 export{CreateTodoButton}
